f1f3740ba294ce582060207de27517d3b4bc696a
Bennett Zhu <bzhu@confluent.io>
2024-12-10 10:29:19 -0800

    try adding main

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bennettzhu1/bazel_go_demo_dep
+
+go 1.18
+
+require github.com/bennettzhu1/bazel_go_demo/libs/hello v1.0.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package bazel_go_demo_dep
+
+import (
+	"github.com/bennettzhu1/bazel_go_demo/libs/hello"
+)
+
+// Greet returns a greeting message
+func Greet() string {
+	return hello.Greet()
+}
